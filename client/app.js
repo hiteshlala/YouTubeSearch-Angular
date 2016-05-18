4230ea726a798c02d54710726e90dd7d2f1ea606
hiteshlala <hiteshlala@gmail.com>
2016-05-17 19:13:51 -0700

    Refactors Custom Directive
    ... so that controller functions are defined in same directive module
    
    On branch refactor-custom-directive
    Changes to be committed:
    modified:   app.js
    modified:   directives/application.js

--- client/app.js
+++ client/app.js
@@ -1,41 +1,5 @@
 angular.module('recastly', [])
 
-.controller('recastly.data', function($scope, youTube) {
-  
-  // Initialize video list and video player with 'the dizi' query string
-  youTube.search(
-    'the dizi',
-    function(data) {
-      $scope.videos = data.items;
-      $scope.playThis(0);
-    }
-  );
-
-  $scope.playThis = function(index) {
-    $scope.currentVideo = index;
-    $scope.getUrl = 'https://www.youtube.com/embed/'+
-      $scope.videos[$scope.currentVideo].id.videoId +'?autoplay=1';
-  };
-
-  $scope.queryYouTube = function(searchQuery) {
-    youTube.search(
-      searchQuery,
-      function(data) {
-        $scope.videos = data.items;
-      }
-    );
-  };
-
-  $scope.query = '';
-
-  $scope.getNewList = function(key) {
-    if(key.which === 13) {
-      $scope.queryYouTube($scope.query);
-      $scope.query = '';
-    }
-  };
-})
-
 .config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
--- client/directives/application.js
+++ client/directives/application.js
@@ -1,9 +1,42 @@
 angular.module('recastly')
-.directive('application', function() {
+.directive('application', function(youTube) {
   return {
     restrict: 'E',
     replace: true,
-    controller: 'recastly.data',
+    controller: function($scope) {
+        // Initialize video list and video player with 'the dizi' query string
+        youTube.search(
+          'the dizi',
+          function(data) {
+            $scope.videos = data.items;
+            $scope.playThis(0);
+          }
+        );
+
+        $scope.playThis = function(index) {
+          $scope.currentVideo = index;
+          $scope.getUrl = 'https://www.youtube.com/embed/'+
+            $scope.videos[$scope.currentVideo].id.videoId +'?autoplay=1';
+        };
+
+        $scope.queryYouTube = function(searchQuery) {
+          youTube.search(
+            searchQuery,
+            function(data) {
+              $scope.videos = data.items;
+            }
+          );
+        };
+
+        $scope.query = '';
+
+        $scope.getNewList = function(key) {
+          if(key.which === 13) {
+            $scope.queryYouTube($scope.query);
+            $scope.query = '';
+          }
+        };
+    },
     templateUrl: 'templates/App.html'
   };
 });
